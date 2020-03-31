cac416107497ce768ece9142799f83a540294de3
Digambar Brahma <digambrahma0@gmail.com>
2020-03-31 20:12:46 +0530

    build

---
+++ precache-manifest.4b124e4ee8dbda40948cce9d854c9219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1147b822f3f86a9e7de89d73a738dbbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e2f14979f961cafae3d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b4a7b29e352418ff9637",
+    "url": "/static/css/main.30501d52.chunk.css"
+  },
+  {
+    "revision": "3e2f14979f961cafae3d",
+    "url": "/static/js/2.f84a4609.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f84a4609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a7b29e352418ff9637",
+    "url": "/static/js/main.11c4f548.chunk.js"
+  },
+  {
+    "revision": "bf99f33f28e8f7510c2a",
+    "url": "/static/js/runtime-main.189ebc62.js"
+  }
+]);
